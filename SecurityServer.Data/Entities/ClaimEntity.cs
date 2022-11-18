64980019f420ede2b65d4e9b6a87f99a493ba351
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2022-11-18 16:12:39 +0100

    ajout des enities

---
+++ SecurityServer.Data/Entities/ClaimEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityServer.Data.Entities
+{
+    public class ClaimEntity
+    {
+        public int Id { get; set; }
+        public string Label { get; set; }
+
+        public ClaimEntity() { }
+
+        public ClaimEntity(int id, string label)
+        {
+            this.Id = id;
+            this.Label = label;
+        }
+    }
+}
---
+++ SecurityServer.Data/Entities/IEntities/IClaimEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityServer.Data.Entities.IEntities
+{
+    public interface IClaimEntity
+    {
+        public int Id { get; set; }
+        public string Label { get; set; }
+    }
+}
