034a2597a73180a012153efb79b12de1aaf7f3b8
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2022-11-18 14:20:41 +0100

    modif paquet

--- SecurityServer.Service/Class1.cs
+++ SecurityServer.Service/Class1.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SecurityServer.Service
+﻿namespace SecurityServer.Service
 {
     public class Class1
     {
+
     }
 }
