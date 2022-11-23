619335c2a1955363d12da280bee8db767fd1a01e
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2022-11-23 12:56:42 +0100

    modification des entities + modification dbContext + ajout du startup

---
+++ SecurityServer.Data/SecurityServerDbContext.cs
@@ -0,0 +1,24 @@
+﻿namespace SecurityServer.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection.Metadata;
+
+    public class SecurityServerDbContext : DbContext
+    {
+        public DbSet<ApplicationEntity>? Applications { get; set; }
+
+        public DbSet<ClaimEntity>? Claims { get; set; }
+
+        public DbSet<RoleEntity>? Roles { get; set; }
+
+        public DbSet<UserEntity>? Users { get; set; }
+
+        public SecurityServerDbContext(DbContextOptions<SecurityServerDbContext> Options)
+            : base(Options)
+        {
+        }
+    }
+}
--- SecurityServer.Entities/IEntities/IClaimEntity.cs
+++ SecurityServer.Entities/IEntities/IClaimEntity.cs
@@ -9,6 +9,7 @@ namespace SecurityServer.Data.Entities.IEntities
     public interface IClaimEntity
     {
         public int Id { get; set; }
-        public string Label { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
