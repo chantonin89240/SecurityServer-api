b101cc23159fb528119caa40a06e1dba090286e7
Kriqx <jeanchristophe.pouzin@diiage.org>
2022-12-12 13:30:29 +0100

    Debut archi user

---
+++ SecurityServer.Data/Repository/Interface/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using SecurityServer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityServer.Data.Repository.Interface
+{
+    public  interface IUserRepository : IBaseRepository<UserEntity>
+    {
+        IEnumerable<UserEntity> Get();
+        UserEntity Get(int id);
+        void Post(UserEntity userEntity);
+        void Update(UserEntity userEntity);
+        void Delete(int id);
+
+    }
+}
